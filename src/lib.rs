c3206e3e2705139950ff538dbb5c86c16dd27d36
Kofi Otuo <otuokofi@outlook.com>
2025-01-23 22:42:23 +0000

    optimise

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use subxt::ext::futures::TryStreamExt;
 use subxt::{OnlineClient, PolkadotConfig};
-use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use wasm_bindgen_test::{console_log, wasm_bindgen_test, wasm_bindgen_test_configure};
 
 
 wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 async fn testing() {
-    return println!("works");
+    return console_log!("works");
     let api: OnlineClient<PolkadotConfig> = OnlineClient::from_url("ws://127.0.0.1:9944").await.map_err(|_| "").unwrap();
 }
