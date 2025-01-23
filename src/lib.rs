9a245c318e4c1c4b6e0ed6f54e1d2ff76efa3765
Kofi Otuo <otuokofi@outlook.com>
2025-01-23 22:44:28 +0000

    optimise

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ use wasm_bindgen_test::{console_log, wasm_bindgen_test, wasm_bindgen_test_config
 wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 async fn testing() {
-    let api: OnlineClient<PolkadotConfig> = OnlineClient::from_url("ws://127.0.0.1:9944").await.map_err(|_| "").unwrap();
-    console_log!("does not work");
+    let api: OnlineClient<PolkadotConfig> = OnlineClient::from_url("ws://127.0.0.1:19944").await.map_err(|_| "").unwrap();
+    console_log!("does not work {:?}",api.genesis_hash());
 }
