ec07ceb887abaf1524d2f5b0f8d5747b0bc9a705
Kofi Otuo <58628654+Kofituo@users.noreply.github.com>
2025-01-24 01:16:17 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 async fn testing() {
-    let api: OnlineClient<PolkadotConfig> = OnlineClient::from_url("ws://127.0.0.1:9944").await.map_err(|_| "").unwrap();
+    let api: OnlineClient<PolkadotConfig> = OnlineClient::from_url("ws://127.0.0.1:9944").await.unwrap();
+    console_log!("does not work {:?}",api.genesis_hash());
 }
